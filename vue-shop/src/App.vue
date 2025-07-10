f5619d7c90515e8554a5a4690072643842399e37
admin <pc@pc.com>
2025-07-10 17:50:18 +0900

    0710 commit

---
+++ vue-shop/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <Header />
+  <router-view />
+  <Footer />
+</template>
+
+<script>
+/*
+    File: App.vue
+    Content: 루트 컴포넌트/ Header, Footer 레이아웃 적용
+    Created: 손이영.
+             Header 수정: 2025.07.11 식으로 변화환경 적어주기 
+    Date: 2025.07.10
+  */
+
+import Header from "./layouts/HeaderLayout.vue";
+import Footer from "./layouts/FooterLayout.vue";
+
+export default {
+  components: { Header, Footer },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ vue-shop/src/layouts/FooterLayout.vue
@@ -0,0 +1,3 @@
+<template>
+  <footer class="mt-5 py-5 bg-dart text-white">&copy;2025</footer>
+</template>
